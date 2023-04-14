d7890fe5a75951668a916bbc08a662694a6edf01
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-04-14 12:50:48 -0700

    half way point

--- followAlong/src/App.js
+++ followAlong/src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 
 import { data } from "./data";
 
 import FamilyTree from "./components/FamilyTree";
 import "./styles.scss";
+
+export const FamilyContext = createContext();
 
 export default function App() {
   const [families] = useState(data);
@@ -11,20 +13,22 @@ export default function App() {
 
   return (
     <div className="App">
-      <section className="header">
-        <h1>Family Trees</h1>
-        {families.map(family => (
-          <button
-            className={`family-button ${family.familyName ===
-              activeFamily.familyName && "active"}`}
-            key={family.familyName}
-            onClick={() => setActiveFamily(family)}
-          >
-            {family.familyName}
-          </button>
-        ))}
-      </section>
-      {activeFamily && <FamilyTree family={activeFamily} />}
+      <FamilyContext.Provider value={activeFamily}>
+        <section className="header">
+          <h1>Family Trees</h1>
+          {families.map(family => (
+            <button
+              className={`family-button ${family.familyName ===
+                activeFamily.familyName && "active"}`}
+              key={family.familyName}
+              onClick={() => setActiveFamily(family)}
+            >
+              {family.familyName}
+            </button>
+          ))}
+        </section>
+        {activeFamily && <FamilyTree family={activeFamily} />}
+      </FamilyContext.Provider>
     </div>
   );
 }
